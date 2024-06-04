13113cd23b422949f4687965c0b5a81ed6b50125
Thomas Luk <thomasluk@gmail.com>
2024-06-04 18:18:06 +0100

    first commit

---
+++ src/test/java/com/thomas/auth/AuthJWT/AuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomas.auth.AuthJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
